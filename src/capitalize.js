00e7a1c21f51f28ee0372413498ad777efcdeb06
Kkuuttii <katerinaugolek@gmail.com>
2022-09-15 22:31:47 +0300

    Урок 2 - зачем и как писать коммиты. Воспроизвела структуру урока, поигралась с ломанием тестов.

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,5 +1,8 @@
 export const capitalize = (text) => {
-    const [firstSymbol = '', ...restSymbols] = text;
-    return `${firstSymbol.toUpperCase()}${restSymbols.join('')}`;
-  };
-  
+  if (text === '') {
+    return '';
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
